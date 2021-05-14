60e572a5bc7c718d4b23738ab6acf1e6881a786a
Ken Schwartz <ken@kennethmschwartz.com>
2021-05-14 08:28:33 -0400

    * Solution for 1.2 * https://lp-prod-resources.s3.amazonaws.com/436/54553/2021-04-16-16-03-58/better-banking-p1m1.zip

---
+++ Author Solution/1_2/src/main/java/io/betterbanking/BetterBankingApplication.java
@@ -0,0 +1,11 @@
+package io.betterbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterBankingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BetterBankingApplication.class, args);
+	}
+}
