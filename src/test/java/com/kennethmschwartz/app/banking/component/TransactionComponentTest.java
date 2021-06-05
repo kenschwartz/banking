548a82a4f2ec6b8bb5affc458ffec813986d07a7
Ken Schwartz <ken@kennethmschwartz.com>
2021-06-05 12:09:31 -0400

    * Added @SpringBootTest(classes = {OpenBankingAppApplication.class}) * Added empty @Test from author code

--- src/test/java/com/kennethmschwartz/app/banking/component/TransactionComponentTest.java
+++ src/test/java/com/kennethmschwartz/app/banking/component/TransactionComponentTest.java
@@ -1,10 +1,9 @@
 package com.kennethmschwartz.app.banking.component;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.boot.web.server.LocalServerPort;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
 import static io.restassured.module.mockmvc.matcher.RestAssuredMockMvcMatchers.*;
@@ -14,4 +13,12 @@ import static io.restassured.module.mockmvc.matcher.RestAssuredMockMvcMatchers.*
 @AutoConfigureMockMvc
 public class TransactionComponentTest {
 
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void testApplicationEndToEnd() {
+        //TODO use MockMvc standaloneSetup method to test just the TransactionController
+        //  This controller should return a HTTP status of 200.
+    }
 }
